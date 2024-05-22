dd1695fc35c448e0d2e96bf3fe9a82be581dc79b
Peter Melick <165929802+pete-tempo@users.noreply.github.com>
2024-05-22 08:46:22 -0400

    Change TempoColorCamera to load PostProcess material in C++

--- Plugins/TempoSensors/Source/TempoSensorsShared/Public/TempoSensorsSettings.h
+++ Plugins/TempoSensors/Source/TempoSensorsShared/Public/TempoSensorsSettings.h
@@ -17,11 +17,17 @@ public:
 	TObjectPtr<UDataTable> GetSemanticLabelTable() const { return SemanticLabelTable.LoadSynchronous(); }
 
 	// Camera
+	TObjectPtr<UMaterialInterface> GetColorAndLabelPostProcessMaterial() const { return ColorAndLabelPostProcessMaterial.LoadSynchronous(); }
 	int32 GetMaxCameraRenderBufferSize() const { return MaxCameraRenderBufferSize; }
 
 private:
+	// The data table from which we will load the mapping from Actor classes to semantic labels.
 	UPROPERTY(EditAnywhere, Config, Category="Labels")
 	TSoftObjectPtr<UDataTable> SemanticLabelTable;
+
+	// The post process material that should be used for TempoColorCamera (which also captures semantic labels).
+	UPROPERTY(EditAnywhere, Config, Category="Camera", meta=( AllowedClasses="/Script/Engine.BlendableInterface", Keywords="PostProcess" ))
+	TSoftObjectPtr<UMaterialInterface> ColorAndLabelPostProcessMaterial;
 
 	// The max number of frames per camera to buffer before dropping.
 	UPROPERTY(EditAnywhere, Config, Category="Camera", AdvancedDisplay)
