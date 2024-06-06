73c186694b7bf247b3131efa875f687bec1ff6b0
Peter Melick <165929802+pete-tempo@users.noreply.github.com>
2024-06-06 08:46:41 -0400

    Remove Tempo Project, Move Plugins to Top Level

--- Source/Tempo/Private/Tempo.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Tempo Simulation, LLC. All Rights Reserved.
-
-#include "Tempo.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Tempo, "Tempo" );
