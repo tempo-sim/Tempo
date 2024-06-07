9d58599cf96d85141826d6531ab2aa8d615c8d03
Peter Melick <165929802+pete-tempo@users.noreply.github.com>
2024-06-07 11:26:05 -0400

    gRPC Symbols Export Dynamically from TempoScripting, not from gRPC (#63)

--- TempoCore/Source/TempoScripting/TempoScripting.Build.cs
+++ TempoCore/Source/TempoScripting/TempoScripting.Build.cs
@@ -55,5 +55,19 @@ public class TempoScripting : TempoModuleRules
 			{
 			}
 			);
+		
+		// These definitions are used when building gRPC and must match here so that the headers
+		// we use in the Tempo build match those in the built libraries exactly.
+		// These are here, not in gRPC.Build.cs, because we import the gRPC symbols statically here
+		// but export them dynamically to other modules.
+		PublicDefinitions.Add("ABSL_BUILD_DLL=1");
+		PublicDefinitions.Add("PROTOBUF_USE_DLLS=1");
+		// Defining these privately will cause symbols to be __declspec(dllexport) within TempoScripring
+		// and __declspec(dllimport) from elsewhere.
+		PrivateDefinitions.Add("LIBPROTOBUF_EXPORTS=1");
+		PrivateDefinitions.Add("LIBPROTOC_EXPORTS=1");
+		PrivateDefinitions.Add("GRPC_DLL_EXPORTS=1");
+		PrivateDefinitions.Add("GRPCXX_DLL_EXPORTS=1");
+		PrivateDefinitions.Add("GPR_DLL_EXPORTS=1");
 	}
 }
