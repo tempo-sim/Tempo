0056b73faa9eecd898ca2f70a64a58dd045fc523
Giovanni Pasteris <gio@temposimulation.com>
2024-07-29 00:47:48 -0400

    Add empty TempoAgents plugin. (#74)

---
+++ TempoAgents/Source/TempoAgentsShared/Private/TempoAgentsShared.cpp
@@ -0,0 +1,19 @@
+﻿#include "TempoAgentsShared.h"
+
+#define LOCTEXT_NAMESPACE "FTempoAgentsSharedModule"
+
+DEFINE_LOG_CATEGORY(LogTempoAgentsShared);
+
+void FTempoAgentsSharedModule::StartupModule()
+{
+    
+}
+
+void FTempoAgentsSharedModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FTempoAgentsSharedModule, TempoAgentsShared)
---
+++ TempoAgents/Source/TempoAgentsShared/TempoAgentsShared.Build.cs
@@ -0,0 +1,26 @@
+﻿using UnrealBuildTool;
+
+public class TempoAgentsShared : ModuleRules
+{
+    public TempoAgentsShared(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore"
+            }
+        );
+    }
+}
