5d3648bcecfdc2ea853c32ba692e55517f628a39
Peter Melick <165929802+pete-tempo@users.noreply.github.com>
2024-08-20 14:21:02 -0400

    Add Empty TempoWorld Plugin and Module

---
+++ TempoWorld/Source/TempoWorld/Private/TempoWorld.cpp
@@ -0,0 +1,19 @@
+﻿// Copyright Tempo Simulation, LLC. All Rights Reserved
+
+#include "TempoWorld.h"
+
+#define LOCTEXT_NAMESPACE "FTempoWorldModule"
+
+void FTempoWorldModule::StartupModule()
+{
+    
+}
+
+void FTempoWorldModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FTempoWorldModule, TempoWorld)
---
+++ TempoWorld/Source/TempoWorld/TempoWorld.Build.cs
@@ -0,0 +1,26 @@
+﻿// Copyright Tempo Simulation, LLC. All Rights Reserved
+
+using UnrealBuildTool;
+
+public class TempoWorld : ModuleRules
+{
+    public TempoWorld(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+            }
+        );
+    }
+}
