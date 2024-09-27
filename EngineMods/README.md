6a4048d40a305703020ff40f21a58d4da9ebedd3
Peter Melick <165929802+pete-tempo@users.noreply.github.com>
2024-09-26 23:39:24 -0400

    Add Workflow for In-Place Engine Mods Including ZoneGraph and UnrealBuildTool Mods (#113)

---
+++ EngineMods/README.md
@@ -0,0 +1,13 @@
+# Tempo Engine Mods
+
+The files in this folder and the associated `Scripts/InstallEngineMods.sh` script are intended to make small modifications to a user's local UnrealEngine installation. Doing so allows Tempo to make minor adjustments to the engine in-place without forcing users to install a custom Tempo engine.
+
+Mods can be in the form of additions, patches, or removals of files. Patches can be stacked, and will be applied in the order they appear in each patch list.
+
+Different versions of each mod may be necessary for different Major.Minor versions of the engine. The `EngineMods.json` file specifies which mod files apply to which versions. Mods will be applied in the order they appear in that file, as later mods may depend on earlier ones.
+
+It is unlikely but not impossible that an Unreal hotfix release may break one of these mods. We will address that if it happens.
+
+> [!Note]
+> You can generate a new patch with a command like this:
+> `diff -urN <original> <new> > <patch>`
---
+++ Scripts/InstallEngineMods.sh
@@ -0,0 +1,217 @@
+#!/usr/bin/env bash
+
+set -e
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+TEMPO_ROOT=$(realpath "$SCRIPT_DIR/..")
+ENGINE_MODS_DIR="$TEMPO_ROOT/EngineMods"
+
+# Check for UNREAL_ENGINE_PATH
+if [ -z ${UNREAL_ENGINE_PATH+x} ]; then
+  echo "Please set UNREAL_ENGINE_PATH environment variable and re-run";
+  exit 1
+fi
+
+if [ ! -f "$UNREAL_ENGINE_PATH/Engine/Source/Programs/UnrealBuildTool/UnrealBuildTool.csproj" ]; then
+  echo "UNREAL_ENGINE_PATH ($UNREAL_ENGINE_PATH) does not seem correct. Please check and re-run";
+  exit 1
+fi
+
+UNREAL_ENGINE_PATH="${UNREAL_ENGINE_PATH//\\//}"
+
+cd "$UNREAL_ENGINE_PATH"
+
+# Find dotnet
+if [[ "$OSTYPE" = "msys" ]]; then
+  DOTNET=$(find ./Engine/Binaries/ThirdParty/DotNet -type f -name dotnet.exe)
+elif [[ "$OSTYPE" = "darwin"* ]]; then
+  DOTNETS=$(find ./Engine/Binaries/ThirdParty/DotNet -type f -name dotnet)
+  ARCH=$(arch)
+  if [[ "$ARCH" = "arm64" ]]; then
+    DOTNET=$(echo "${DOTNETS[@]}" | grep -E "mac-arm64/dotnet")
+  elif [[ "$ARCH" = "i386" ]]; then
+    DOTNET=$(echo "${DOTNETS[@]}" | grep -E "mac-x64/dotnet")
+  fi
+elif [[ "$OSTYPE" = "linux-gnu"* ]]; then
+  DOTNET=$(find ./Engine/Binaries/ThirdParty/DotNet -type f -name dotnet)
+fi
+
+# Get engine version (e.g. 5.4)
+if [ -f "$UNREAL_ENGINE_PATH/Engine/Intermediate/Build/BuildRules/UE5RulesManifest.json" ]; then
+  VERSION_WITH_HOTFIX=$(jq -r '.EngineVersion' "$UNREAL_ENGINE_PATH/Engine/Intermediate/Build/BuildRules/UE5RulesManifest.json")
+  VERSION="${VERSION_WITH_HOTFIX%.*}"
+fi
+
+MOD_ADD() {
+  local TYPE=$1
+  local ROOT=$2
+  local MOD=$3
+  local SRC="$ENGINE_MODS_DIR/$ROOT/$MOD"
+  local MOD_NO_VERSION="${MOD%.*}"
+  local DEST="$UNREAL_ENGINE_PATH/$ROOT/$MOD_NO_VERSION"
+  # Copy if missing or stale
+  if [[ ! -f "$DEST" || $(diff "$SRC" "$DEST" | wc -l) -gt 0 ]]; then
+    echo "Installing $MOD"
+    mkdir -p "$(dirname "$DEST")"
+    cp "$SRC" "$DEST"
+    return 1
+  fi
+  return 0
+}
+
+MOD_PATCH() {
+  local TYPE=$1
+  local ROOT=$2
+  local PATCHES=("${@:3}")
+  local DEST
+  DEST="$UNREAL_ENGINE_PATH/$ROOT"
+  
+  if [ ! -d "$DEST" ]; then
+    echo "Not patching $DEST since it was not found in the expected location"
+    return 0
+  fi
+
+  # Reverse the order of patches
+  local REVERSED_PATCHES=($(printf '%s\n' "${PATCHES[@]}" | sed -n '1!G;h;$p'))
+  local PATCHES_APPLIED=0
+
+  # Find the last applied patch (if any)
+  cd "$DEST"
+  local LAST_APPLIED_INDEX=$((${#PATCHES[@]} - 1))
+  for ((i=0; i<${#REVERSED_PATCHES[@]}; i++)); do
+    local PATCH="$ENGINE_MODS_DIR/$ROOT/${REVERSED_PATCHES[i]//\'/}"
+    # To check if the patch has been applied, check if it can be *reversed* (if not, it hasn't been applied).
+    # https://unix.stackexchange.com/questions/55780/check-if-a-file-or-folder-has-been-patched-already
+    if patch -R -p0 -s -f --dry-run <"$PATCH" >/dev/null 2>&1; then
+      echo "Patch $(basename "$PATCH") already applied"
+      LAST_APPLIED_INDEX=$((${#PATCHES[@]} - i))
+      break
+    fi
+  done
+
+  # Apply all patches after the last applied one
+  for ((i=LAST_APPLIED_INDEX; i<${#PATCHES[@]}; i++)); do
+    local PATCH="$ENGINE_MODS_DIR/$ROOT/${PATCHES[i]//\'/}"
+    echo "Applying Patch $(basename "$PATCH")"
+    if ! patch -p0 <"$PATCH" >/dev/null 2>&1; then
+      echo "Failed to apply patch: $PATCH"
+      exit 1
+    fi
+    PATCHES_APPLIED=$((PATCHES_APPLIED + 1))
+  done
+  
+  if [ $PATCHES_APPLIED -gt 0 ]; then
+    return 1
+  fi
+
+  return 0
+}
+
+MOD_REMOVE() {
+  local TYPE=$1
+  local ROOT=$2
+  local MOD=$3
+  local DEST="$UNREAL_ENGINE_PATH/$ROOT/$MOD"
+  if [ -f "$DEST" ]; then
+    # Confirm with the user before removing
+    read -r -p "Stale $TYPE engine mod $DEST found. Remove it? (Y/N) " DO_REMOVE
+    if [[ "$DO_REMOVE" =~ [Yy] ]]; then
+      echo "Removing stale mod $DEST"
+      rm -rf "$DEST"
+      return 1      
+    fi
+    echo "User elected not to remove $DEST."
+    exit 1
+  fi
+  return 0
+}
+
+REBUILD_PLUGIN() {
+  ROOT="$1"
+  PLUGIN_NAME="${ROOT##*/}"
+  PLUGIN_DIR="$UNREAL_ENGINE_PATH/$ROOT"
+  
+  if [ ! -d "$PLUGIN_DIR" ]; then
+    echo "Plugin $PLUGIN_DIR was not found in the expected location."
+    exit 1
+  fi
+
+  echo -e "Rebuilding plugin $PLUGIN_NAME with Tempo mods"
+
+  TEMP_DIR=$(mktemp -d)
+  cd "$UNREAL_ENGINE_PATH"
+  if [[ "$OSTYPE" = "msys" ]]; then
+    ./Engine/Build/BatchFiles/RunUAT.bat BuildPlugin -Plugin="$ROOT/$PLUGIN_NAME.uplugin" -Package="$TEMP_DIR" -Rocket -TargetPlatforms=Win64
+  elif [[ "$OSTYPE" = "darwin"* ]]; then
+    ./Engine/Build/BatchFiles/RunUAT.sh BuildPlugin -Plugin="$ROOT/$PLUGIN_NAME.uplugin" -Package="$TEMP_DIR" -Rocket -TargetPlatforms=Mac
+  elif [[ "$OSTYPE" = "linux-gnu"* ]]; then
+    ./Engine/Build/BatchFiles/RunUAT.sh BuildPlugin -Plugin="$ROOT/$PLUGIN_NAME.uplugin" -Package="$TEMP_DIR" -Rocket -TargetPlatforms=Linux
+  fi
+
+  # Copy resulting Binaries and Intermediate folders
+  cp -r "$TEMP_DIR/Binaries" "$TEMP_DIR/Intermediate" "$PLUGIN_DIR"
+  
+  # Clean up
+  rm -rf "$TEMP_DIR"
+
+  echo -e "\nSuccessfully rebuilt plugin $PLUGIN_NAME with Tempo mods\n"
+}
+
+REBUILD_UBT() {
+  cd "$UNREAL_ENGINE_PATH"
+    
+  if [ -z ${DOTNET+x} ]; then
+    echo -e "Unable rebuild UnrealBuildTool with Tempo mods. Couldn't find dotnet.\n"
+    exit 1
+  fi
+  
+  eval "$DOTNET" build "./Engine/Source/Programs/UnrealBuildTool/UnrealBuildTool.csproj" -c Development
+  eval "$DOTNET" build "./Engine/Source/Programs/AutomationTool/AutomationTool.csproj" -c Development
+}
+
+TYPES=($(jq -r --arg version "$VERSION" '.[$version][].Type // [] | @sh' "$TEMPO_ROOT/EngineMods/EngineMods.json" | tr -d \'))
+for TYPE in "${TYPES[@]}"; do
+  # Remove any trailing carriage returns
+  TYPE="${TYPE%$'\r'}"
+  ROOT=$(jq -r --arg version "$VERSION" --arg type "$TYPE" '.[$version][] | select(.Type == $type) | .Root' "$TEMPO_ROOT/EngineMods/EngineMods.json" | tr -d \')
+  # Remove any trailing carriage returns
+  ROOT="${ROOT%$'\r'}"
+    
+  NEEDS_REBUILD='N'
+  
+  ADDS=$(jq -r --arg version "$VERSION" --arg type "$TYPE" '.[$version][] | select(.Type == $type) | .Add // [] | join(" ")' "$TEMPO_ROOT/EngineMods/EngineMods.json" | tr -d \')
+  read -ra ADDS <<< "$ADDS"
+  for ADD in "${ADDS[@]}"; do
+    if ! MOD_ADD "$TYPE" "$ROOT" "$ADD"; then
+      NEEDS_REBUILD='Y'
+    fi
+  done
+
+  while read -r PATCHES; do
+    read -ra PATCHES <<< "$PATCHES"
+    if ! MOD_PATCH "$TYPE" "$ROOT" "${PATCHES[@]}"; then
+      NEEDS_REBUILD='Y'
+    fi
+  done <<< "$(jq -r --arg version "$VERSION" --arg type "$TYPE" '.[$version][] | select(.Type == $type) | .Patch[] | @sh' "$TEMPO_ROOT/EngineMods/EngineMods.json")" 
+
+  REMOVES=$(jq -r --arg version "$VERSION" --arg type "$TYPE" '.[$version][] | select(.Type == $type) | .Remove // [] | join(" ")' "$TEMPO_ROOT/EngineMods/EngineMods.json" | tr -d \')
+  read -ra REMOVES <<< "$REMOVES"
+  for REMOVE in "${REMOVES[@]}"; do
+    if ! MOD_REMOVE "$TYPE" "$ROOT" "$REMOVE"; then
+      NEEDS_REBUILD='Y'
+    fi
+  done
+  
+  if [ "$NEEDS_REBUILD" = 'Y' ] || [ "${*: -1}" = "-force" ]; then
+    if [ "$TYPE" = "Plugin" ]; then
+      echo "Rebuilding plugin $ROOT with Tempo mods"
+      REBUILD_PLUGIN "$ROOT"
+    elif [ "$TYPE" = "UnrealBuildTool" ]; then
+      echo "Rebuilding UnrealBuildTool with Tempo mods"
+      REBUILD_UBT
+    else
+      echo "Unhandled mod type: $TYPE"
+      exit 1
+    fi
+  fi
+done
