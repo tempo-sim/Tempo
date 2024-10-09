032eb17d548cd3cb57f3dffd96e01894261dffd3
Peter Melick <pete@temposimulation.com>
2024-10-09 17:14:02 -0400

    Add Special ROS Preprocessor Defines

--- TempoROSBridge/Source/TempoScriptingROSBridge/TempoScriptingROSBridge.Build.cs
+++ TempoROSBridge/Source/TempoScriptingROSBridge/TempoScriptingROSBridge.Build.cs
@@ -30,6 +30,13 @@ public class TempoScriptingROSBridge : ModuleRules
             }
         );
         
+        if (Target.Platform == UnrealTargetPlatform.Win64)
+        {
+            PrivateDefinitions.Add("ROSIDL_TYPESUPPORT_FASTRTPS_CPP_BUILDING_DLL_tempo_scripting_ros_bridge=1");
+            PrivateDefinitions.Add("ROSIDL_TYPESUPPORT_CPP_BUILDING_DLL=1");
+            PrivateDefinitions.Add("ROSIDL_TYPESUPPORT_INTROSPECTION_CPP_BUILDING_DLL=1");
+        }
+        
         bEnableExceptions = true;
     }
 }
