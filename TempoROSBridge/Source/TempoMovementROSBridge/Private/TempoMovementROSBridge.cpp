9dd61cc7007c6f78f1505008dec5cbc9cbf97b9a
Peter Melick <165929802+pete-tempo@users.noreply.github.com>
2024-10-16 16:32:45 -0400

    Update TempoROSBridge for TempoROS Windows Support (#138)

---
+++ TempoROSBridge/Source/TempoMovementROSBridge/Private/TempoMovementROSBridge.cpp
@@ -0,0 +1,17 @@
+﻿#include "TempoMovementROSBridge.h"
+
+#define LOCTEXT_NAMESPACE "FTempoMovementROSBridgeModule"
+
+void FTempoMovementROSBridgeModule::StartupModule()
+{
+    
+}
+
+void FTempoMovementROSBridgeModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FTempoMovementROSBridgeModule, TempoMovementROSBridge)
