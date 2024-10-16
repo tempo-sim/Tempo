71967325bfe5410ab3e9c0003f7c9a49f551e3e7
Peter Melick <165929802+pete-tempo@users.noreply.github.com>
2024-10-16 14:33:42 -0400

    Add Tempo Lidar

---
+++ TempoSensors/Source/TempoLidar/Private/TempoLidar.cpp
@@ -0,0 +1,17 @@
+﻿#include "TempoLidar.h"
+
+#define LOCTEXT_NAMESPACE "FTempoLidarModule"
+
+void FTempoLidarModule::StartupModule()
+{
+    
+}
+
+void FTempoLidarModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FTempoLidarModule, TempoLidar)
