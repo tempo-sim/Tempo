75804f6ca40d8c31ec17567ef0f1432e8b8ad3c2
Peter Melick <165929802+pete-tempo@users.noreply.github.com>
2024-10-24 11:41:47 -0400

    Ranges Not Points

--- TempoSensors/Source/TempoLidar/Private/TempoLidar.cpp
+++ TempoSensors/Source/TempoLidar/Private/TempoLidar.cpp
@@ -24,6 +24,11 @@ void UTempoLidar::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedE
 {
 	Super::PostEditChangeProperty(PropertyChangedEvent);
 
+	UpdateComputedProperties();
+}
+
+void UTempoLidar::UpdateComputedProperties()
+{
 	// Special case for 1 vertical beam? Special lidar?
 
 	FOVAngle = HorizontalFOV;
@@ -31,18 +36,17 @@ void UTempoLidar::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedE
 	VerticalSpacing = VerticalFOV / VerticalBeams;
 	const float TanHFOVOverTwo = FMath::Tan(FMath::DegreesToRadians(HorizontalFOV / 2.0));
 	const float DistortionFactorContinuous = FMath::Sqrt((TanHFOVOverTwo * TanHFOVOverTwo) + 1);
-
 	const float UndistortedAspectRatio = FMath::Tan(FMath::DegreesToRadians(VerticalFOV / 2.0)) / FMath::Tan(FMath::DegreesToRadians(HorizontalFOV / 2.0));
-
 	SizeXY = FIntPoint(HorizontalBeams, FMath::CeilToInt32(DistortionFactorContinuous * UndistortedAspectRatio * HorizontalBeams));
 	DistortionFactor = SizeXY.Y / (UndistortedAspectRatio * HorizontalBeams);
-
 	DistortedVerticalFOV = FMath::RadiansToDegrees(2.0 * FMath::Atan(FMath::Tan(FMath::DegreesToRadians(VerticalFOV) / 2.0) * DistortionFactor));
 }
 
 void UTempoLidar::BeginPlay()
 {
 	Super::BeginPlay();
+
+	UpdateComputedProperties();
 
 	const UTempoSensorsSettings* TempoSensorsSettings = GetDefault<UTempoSensorsSettings>();
 	check(TempoSensorsSettings);
@@ -51,7 +55,7 @@ void UTempoLidar::BeginPlay()
 	{
 		PostProcessMaterialInstance = UMaterialInstanceDynamic::Create(PostProcessMaterial.Get(), this);
 		const float VerticalFOVAngle = 2.0 * FMath::Atan(FMath::Tan(FMath::DegreesToRadians(FOVAngle) / 2.0) * (SizeXY.Y / SizeXY.X));
-		PostProcessMaterialInstance->SetScalarParameterValue(TEXT("VerticalFOV"), VerticalFOVAngle);
+		PostProcessMaterialInstance->SetScalarParameterValue(TEXT("VerticalFOV"), DistortedVerticalFOV);
 		PostProcessMaterialInstance->SetScalarParameterValue(TEXT("HorizontalFOV"), FOVAngle);
 	}
 	else
@@ -71,7 +75,9 @@ FTextureRead* UTempoLidar::MakeTextureRead() const
 {
 	check(GetWorld());
 
-	return static_cast<FTextureRead*>(new TTextureRead<FDepthPixel>(SizeXY, SequenceId, GetWorld()->GetTimeSeconds(), GetOwnerName(), GetSensorName(), MinRange, MaxRange));
+	return new TTextureRead<FDepthPixel>(SizeXY, SequenceId, GetWorld()->GetTimeSeconds(),
+		GetOwnerName(), GetSensorName(), MinRange, MaxRange, HorizontalBeams, VerticalBeams,
+		DistortedVerticalFOV, VerticalFOV, UpsamplingFactor);
 }
 
 void TTextureRead<FDepthPixel>::RespondToRequests(const TArray<FLidarScanRequest>& Requests, float TransmissionTime) const
@@ -81,12 +87,36 @@ void TTextureRead<FDepthPixel>::RespondToRequests(const TArray<FLidarScanRequest
 		TRACE_CPUPROFILER_EVENT_SCOPE(TempoCameraDecodeColor);
 		std::vector<char> ImageData;
 		ImageData.reserve(Image.Num() * 3);
-		for (const auto& Pixel : Image)
+
+		const float VerticalOffset = DistortedVerticalFOV / 2.0 - VerticalFOV / 2.0;
+		const float VerticalSpacing = VerticalFOV / (VerticalBeams - 1);
+		auto RangeAtBeam = [this, VerticalOffset, VerticalSpacing] (int32 HorizontalBeam, int32 VerticalBeam) -> float
 		{
-			TempoLidar::LidarReturn Return;
-			// Return.set_allocated_point()
-			// ScanSegment.add_returns(Pixel.GetDepth(MinRange, MaxRange));
+			const int32 Column = HorizontalBeam;
+			const float Row = VerticalOffset + VerticalBeam * VerticalSpacing;
+			const float RangeAbove = Image[Column + HorizontalBeams * FMath::FloorToInt32(Row)].GetDepth(MinRange, MaxRange);
+			const float RangeBelow = Image[Column + HorizontalBeams * FMath::CeilToInt32(Row)].GetDepth(MinRange, MaxRange);
+			const float Alpha = Row - FMath::FloorToInt32(Row);
+			if (FMath::Abs(RangeAbove - RangeBelow) > 10.0)
+			{
+				return Alpha > 0.5 ? RangeBelow : RangeAbove;
+			}
+			else
+			{
+				return RangeAbove * (1.0 - Alpha) + RangeBelow * Alpha;
+			}
+		};
+
+		ScanSegment.mutable_returns()->Reserve(HorizontalBeams * VerticalBeams);
+		for (int32 HorizontalBeam = 0; HorizontalBeam < HorizontalBeams; ++HorizontalBeam)
+		{
+			for (int32 VerticalBeam = 0; VerticalBeam < VerticalBeams; ++VerticalBeam)
+			{
+				auto* Return = ScanSegment.add_returns();
+				Return->set_range(RangeAtBeam(HorizontalBeam, VerticalBeam) / 100.0);
+			}
 		}
+
 		ScanSegment.mutable_header()->set_sequence_id(SequenceId);
 		ScanSegment.mutable_header()->set_capture_time(CaptureTime);
 		ScanSegment.mutable_header()->set_transmission_time(TransmissionTime);
