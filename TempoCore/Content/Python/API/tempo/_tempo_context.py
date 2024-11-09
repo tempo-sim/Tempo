40f97a6fece79757564087e34f085c52bd1b7e00
Peter Melick <165929802+pete-tempo@users.noreply.github.com>
2024-11-09 15:01:55 -0500

    Make Async Handling More Robust

--- TempoCore/Content/Python/API/tempo/_tempo_context.py
+++ TempoCore/Content/Python/API/tempo/_tempo_context.py
@@ -6,42 +6,63 @@ import os
 import sys
 
 
-def get_event_loop():
+def run_async(coroutine):
+    """
+    A helpful utility to run an async command from any
+    (synchronous or asynchronous) context.
+    """
     try:
-        loop = asyncio.get_running_loop()
+        # This will raise RuntimeError if we are in a synchronous context
+        asyncio.get_running_loop()
+        # If it didn't raise we are in an asynchronous context and can just return the coroutine
+        return coroutine
     except RuntimeError:
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-    return loop
+        # This is a synchronous context, but there still might be a loop running elsewhere in the process
+        # There does not seem to be a way of detecting this situation without accessing the protected members below
+        running_loop = asyncio.get_event_loop_policy()._local._loop
+        if running_loop is not None:
+            # Submit the coroutine to that loop, if it exists
+            running_loop.run_until_complete(coroutine)
+        else:
+            # Otherwise, use asyncio.run to create a new loop and close it after running
+            asyncio.run(coroutine)
 
 
 def set_server(address="localhost", port=10001):
-    loop = get_event_loop()
-    loop.run_until_complete(tempo_context()._set_server(address, port))
+    run_async(tempo_context()._set_server(address, port))
 
 
 class TempoContext(object):
     def __init__(self):
         self.server_address = "localhost"
         self.server_port = "10001"
-        self._init_channel()
         self.stubs = {}
+        self.channel = None
+        self.channel_loop = None
+        run_async(self._init_channel())
 
-    def get_stub(self, stub_class):
+    async def get_stub(self, stub_class):
+        if self.channel is None:
+            await self._init_channel()
+        elif self.channel_loop is not asyncio.get_running_loop():
+            # We are in a different loop than the one our channel is running on
+            # Reinitialize our channel on the current one
+            await self._init_channel()
         return self.stubs.setdefault(stub_class, stub_class(self.channel))
 
     async def _set_server(self, address="localhost", port="10001"):
         self.server_address = address
         self.server_port = port
-        self._init_channel()
+        await self._init_channel()
 
-    def _init_channel(self):
+    async def _init_channel(self):
+        self.stubs = {}
+        self.channel_loop = asyncio.get_running_loop()
         self.channel = grpc.aio.insecure_channel("{}:{}".format(self.server_address, self.server_port),
                                                  options=[
                                                      ('grpc.max_receive_message_length', 1000000000), # 1Gb
                                                  ]
                                                  )
-        self.stubs = {}
 
 
 # "Singleton Factory" https://stackoverflow.com/questions/12305142/issue-with-singleton-python-call-two-times-init
