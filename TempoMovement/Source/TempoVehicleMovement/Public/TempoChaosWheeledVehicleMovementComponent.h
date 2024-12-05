405a932574d984a627c3c5ca9e322b270dfcf27d
Peter Melick <165929802+pete-tempo@users.noreply.github.com>
2024-12-05 15:29:59 -0500

    Manual Vehicle Control

---
+++ TempoMovement/Source/TempoVehicleMovement/Public/TempoChaosWheeledVehicleMovementComponent.h
@@ -0,0 +1,26 @@
+// Copyright Tempo Simulation, LLC. All Rights Reserved
+
+#pragma once
+
+#include "TempoVehicleMovementInterface.h"
+#include "TempoMovementInterface.h"
+
+#include "ChaosWheeledVehicleMovementComponent.h"
+#include "CoreMinimal.h"
+
+#include "TempoChaosWheeledVehicleMovementComponent.generated.h"
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class TEMPOVEHICLEMOVEMENT_API UTempoChaosWheeledVehicleMovementComponent : public UChaosWheeledVehicleMovementComponent, public ITempoVehicleMovementInterface, public ITempoMovementInterface
+{
+	GENERATED_BODY()
+
+public:
+	UTempoChaosWheeledVehicleMovementComponent();
+
+	virtual float GetLinearVelocity() override { return VehicleState.ForwardSpeed; }
+
+	virtual FVector GetAngularVelocity() override { return VehicleState.VehicleWorldAngularVelocity; }
+	
+	virtual void HandleDrivingCommand(const FDrivingCommand& Command) override;
+};
