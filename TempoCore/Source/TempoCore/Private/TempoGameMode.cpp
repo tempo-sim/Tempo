fc163502b37246d0af74c6cb3aa0a35532f96528
Peter Melick <165929802+pete-tempo@users.noreply.github.com>
2024-12-09 10:20:40 -0500

    Block for Level Streaming Completed on Deferred Level Load (#171)

--- TempoCore/Source/TempoCore/Private/TempoGameMode.cpp
+++ TempoCore/Source/TempoCore/Private/TempoGameMode.cpp
@@ -12,6 +12,8 @@ void ATempoGameMode::StartPlay()
 	UTempoCoreServiceSubsystem* TempoCoreServiceSubsystem = UGameInstance::GetSubsystem<UTempoCoreServiceSubsystem>(GetGameInstance());
 	if (TempoCoreServiceSubsystem)
 	{
+		// Wait until level streaming is up to date (and all streamed actors are loaded)
+		GEngine->BlockTillLevelStreamingCompleted(GetWorld());
 		TempoCoreServiceSubsystem->OnLevelLoaded();
 		if (TempoCoreServiceSubsystem->GetDeferBeginPlay())
 		{
