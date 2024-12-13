9fd8be2911b559c579e67bafdcc6ba265277c420
Peter Melick <165929802+pete-tempo@users.noreply.github.com>
2024-12-13 09:41:58 -0500

    Add TempoCoreEditor Module (#172)

---
+++ TempoCore/Source/TempoCoreEditor/TempoCoreEditor.Build.cs
@@ -0,0 +1,40 @@
+﻿// Copyright Tempo Simulation, LLC. All Rights Reserved
+
+using UnrealBuildTool;
+
+public class TempoCoreEditor : TempoModuleRules
+{
+    public TempoCoreEditor(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+                "TempoScripting",
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "EngineSettings",
+                "Slate",
+                "SlateCore"
+            }
+        );
+
+        if (Target.bBuildEditor)
+        {
+            PrivateDependencyModuleNames.AddRange(
+                new string[]
+                {
+                    "UnrealEd",
+                }
+            );
+        }
+    }
+}
