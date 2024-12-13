35d0c9200b4d24da724b075431693658fd326a3a
Peter Melick <165929802+pete-tempo@users.noreply.github.com>
2024-12-13 09:41:58 -0500

    Add TempoCoreEditor Module (#172)

---
+++ TempoCore/Source/TempoCoreEditor/Private/TempoCoreEditor.cpp
@@ -0,0 +1,19 @@
+﻿// Copyright Tempo Simulation, LLC. All Rights Reserved
+
+#include "TempoCoreEditor.h"
+
+#define LOCTEXT_NAMESPACE "FTempoCoreEditorModule"
+
+void FTempoCoreEditorModule::StartupModule()
+{
+    
+}
+
+void FTempoCoreEditorModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FTempoCoreEditorModule, TempoCoreEditor)
