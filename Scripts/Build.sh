f358a45ddcb77f3f9f89e204a8a90cdcfaa780f9
Peter Melick <165929802+pete-tempo@users.noreply.github.com>
2025-01-15 18:27:07 -0500

    Move Convenience Scripts into Tempo, Add RenamePakChunks Script

---
+++ Scripts/Build.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -e
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+PROJECT_ROOT=$("$SCRIPT_DIR"/FindProjectRoot.sh)
+cd "$PROJECT_ROOT"
+PROJECT_NAME=$(find . -maxdepth 1 -name "*.uproject" -exec basename {} .uproject \;)
+
+# Check for UNREAL_ENGINE_PATH
+if [ -z ${UNREAL_ENGINE_PATH+x} ]; then
+  echo "Please set UNREAL_ENGINE_PATH environment variable and re-run";
+  exit 1
+fi
+
+PLATFORM=""
+if [[ "$OSTYPE" = "msys" ]]; then
+  PLATFORM="Win64"
+elif [[ "$OSTYPE" = "darwin"* ]]; then
+  PLATFORM="Mac"
+elif [[ "$OSTYPE" = "linux-gnu"* ]]; then
+  PLATFORM="Linux"
+else
+  echo "Unsupported platform"
+  exit 1
+fi
+
+cd "$UNREAL_ENGINE_PATH"
+if [ "$PLATFORM" = "Win64" ]; then
+  # Windows build script is a little different
+  ./Engine/Build/BatchFiles/Build.bat "${PROJECT_NAME}Editor" Development $PLATFORM -Project="$PROJECT_ROOT/$PROJECT_NAME.uproject" -WaitMutex -FromMsBuild "$@"
+else
+  ./Engine/Build/BatchFiles/$PLATFORM/Build.sh "${PROJECT_NAME}Editor" Development $PLATFORM -Project="$PROJECT_ROOT/$PROJECT_NAME.uproject" -buildscw "$@"
+fi
---
+++ Scripts/Package.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+set -e
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+PROJECT_ROOT=$("$SCRIPT_DIR"/FindProjectRoot.sh)
+cd "$PROJECT_ROOT"
+PROJECT_NAME=$(find . -maxdepth 1 -name "*.uproject" -exec basename {} .uproject \;)
+
+# Check for UNREAL_ENGINE_PATH
+if [ -z ${UNREAL_ENGINE_PATH+x} ]; then
+  echo "Please set UNREAL_ENGINE_PATH environment variable and re-run";
+  exit 1
+fi
+
+HOST_PLATFORM=""
+TARGET_PLATFORM=""
+if [[ "$OSTYPE" = "msys" ]]; then
+  HOST_PLATFORM="Win64"
+  if [ "$1" = "Linux" ]; then
+    if [ -z ${LINUX_MULTIARCH_ROOT+x} ]; then
+      echo "LINUX_MULTIARCH_ROOT not set, cannot cross-compile for Linux"
+      exit 1
+    else
+      TARGET_PLATFORM="Linux"
+    fi
+  else
+    TARGET_PLATFORM="Win64"
+  fi
+elif [[ "$OSTYPE" = "darwin"* ]]; then
+  HOST_PLATFORM="Mac"
+  TARGET_PLATFORM="Mac"
+elif [[ "$OSTYPE" = "linux-gnu"* ]]; then
+  HOST_PLATFORM="Linux"
+  TARGET_PLATFORM="Linux"
+else
+  echo "Unsupported platform"
+  exit 1
+fi
+
+cd "$UNREAL_ENGINE_PATH"
+if [ "$HOST_PLATFORM" = "Win64" ]; then
+  ./Engine/Build/BatchFiles/RunUAT.bat Turnkey -command=VerifySdk -platform=$TARGET_PLATFORM -UpdateIfNeeded -project="$PROJECT_ROOT/$PROJECT_NAME.uproject" BuildCookRun -nop4 -utf8output -nocompileeditor \
+  -skipbuildeditor -cook -target="$PROJECT_NAME" -unrealexe="UnrealEditor-Cmd.exe" -platform=$TARGET_PLATFORM -project="$PROJECT_ROOT/$PROJECT_NAME.uproject" -installed -stage -package -pak \
+  -build -iostore -prereqs -stagingirectory="$PROJECT_ROOT/Packaged" -clientconfig=Development -ScriptDir="$PROJECT_ROOT/Plugins/Tempo/TempoROS/Scripts" "$@"
+elif [ "$HOST_PLATFORM" = "Mac" ]; then
+  ./Engine/Build/BatchFiles/RunUAT.sh Turnkey -command=VerifySdk -platform=$TARGET_PLATFORM -UpdateIfNeeded -project="$PROJECT_ROOT/$PROJECT_NAME.uproject" BuildCookRun -nop4 -utf8output -nocompileeditor \
+  -skipbuildeditor -cook -target="$PROJECT_NAME" -unrealexe="UnrealEditor-Cmd" -platform=$TARGET_PLATFORM -project="$PROJECT_ROOT/$PROJECT_NAME.uproject" -installed -stage -archive -package -pak \
+  -build -iostore -prereqs -archivedirectory="$PROJECT_ROOT/Packaged" -clientconfig=Development -ScriptDir="$PROJECT_ROOT/Plugins/Tempo/TempoROS/Scripts" "$@"
+elif [ "$HOST_PLATFORM" = "Linux" ]; then
+  ./Engine/Build/BatchFiles/RunUAT.sh Turnkey -command=VerifySdk -platform=$TARGET_PLATFORM -UpdateIfNeeded -project="$PROJECT_ROOT/$PROJECT_NAME.uproject" BuildCookRun -nop4 -utf8output -nocompileeditor \
+  -skipbuildeditor -cook -target="$PROJECT_NAME" -unrealexe="UnrealEditor" -platform=$TARGET_PLATFORM -project="$PROJECT_ROOT/$PROJECT_NAME.uproject" -installed -stage -package -pak \
+  -build -iostore -prereqs -stagingdirectory="$PROJECT_ROOT/Packaged" -clientconfig=Development -ScriptDir="$PROJECT_ROOT/Plugins/Tempo/TempoROS/Scripts" "$@"
+else
+  echo "Unsupported platform"
+  exit 1
+fi
+
+# Copy cook metadata (including chunk manifests) to the package directory
+cp -r "$PROJECT_ROOT/Saved/Cooked/$TARGET_PLATFORM/$PROJECT_NAME/Metadata" "$PROJECT_ROOT/Packaged"
+
+# Rename pak chunks by the levels they contain (unless told not to)
+if [[ $* != *skippakchunkrename* ]]; then
+  eval "$SCRIPT_DIR"/RenamePakChunks.sh "$PROJECT_ROOT/Packaged" "$PROJECT_ROOT/Packaged/Metadata"
+fi
