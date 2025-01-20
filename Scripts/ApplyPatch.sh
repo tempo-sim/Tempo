31afad0f8d3c7aebc4b43114c2953e057c071b76
Peter Melick <165929802+pete-tempo@users.noreply.github.com>
2025-01-20 15:48:50 -0500

    Make Tempo GitHub Workflow Reusable (#192)

---
+++ Scripts/ApplyPatch.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Check arguments
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 <Package directory> <Patch>"
+  exit 1
+fi
+
+if [ ! -d "$1" ]; then
+  echo "Package directory $1 does not exist"
+  exit 1
+fi
+
+if [ ! -f "$2" ]; then
+  echo "Patch file $2 does not exist"
+  exit 1
+fi
+
+PACKAGE_PATH=$(realpath "$1")
+PATCH_FILE=$(realpath "$2")
+
+# PAK_PATH will be in different locations depending on several factors including platform.
+# So just find it. It will be the only directory with pak files. This will quit after the first one it finds.
+PAK_PATH=$(find "$PACKAGE_PATH" -name "*.pak" -exec dirname {} \; -quit)
+
+if [ ! -d "$PAK_PATH" ]; then
+  echo "Could not find pak path"
+  exit 1
+fi
+
+unzip -q -o "$PATCH_FILE" -d "$PAK_PATH"
