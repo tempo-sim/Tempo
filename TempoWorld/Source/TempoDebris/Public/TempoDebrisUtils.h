e9be8c6389871cbf143ed998ff58125a82cbfaa9
Peter Melick <165929802+pete-tempo@users.noreply.github.com>
2025-01-31 15:29:28 -0500

    Add Utility to Spawn HISM Instances Along Spline

---
+++ TempoWorld/Source/TempoDebris/Public/TempoDebrisUtils.h
@@ -0,0 +1,47 @@
+// Copyright Tempo Simulation, LLC. All Rights Reserved
+
+#pragma once
+
+#include "Kismet/BlueprintFunctionLibrary.h"
+
+#include "TempoDebrisUtils.generated.h"
+
+class USplineComponent;
+class UFoliageType;
+class UHierarchicalInstancedStaticMeshComponent;
+
+USTRUCT(BlueprintType)
+struct FHISMSplineInstanceInfo
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UStaticMesh* StaticMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Spacing = 0.0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Width = 0.0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FVector LocationOffset = FVector::ZeroVector;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FVector RandomLocationRange = FVector::ZeroVector;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FVector RandomScaleRange = FVector::ZeroVector;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool bRandomYaw = true;
+};
+
+UCLASS()
+class TEMPODEBRIS_API UTempoDebrisUtils : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintCallable, Category = "TempoDebris|Utils", meta = (WorldContext = "WorldContextObject"))
+	static void SpawnHISMInstancesAlongSpline(USplineComponent* Spline, UHierarchicalInstancedStaticMeshComponent* HISMComponent, const FHISMSplineInstanceInfo& HISMSplineInstanceInfo);
+};
