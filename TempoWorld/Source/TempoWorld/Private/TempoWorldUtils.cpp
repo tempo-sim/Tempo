d1bcd5a715d1c1ad8e7976b1488411df3e453b09
Peter Melick <165929802+pete-tempo@users.noreply.github.com>
2025-02-04 12:17:14 -0500

    Enable Setting Asset Properties in TempoWorld (#203)

--- TempoWorld/Source/TempoWorld/Private/TempoWorldUtils.cpp
+++ TempoWorld/Source/TempoWorld/Private/TempoWorldUtils.cpp
@@ -20,3 +20,26 @@ AActor* GetActorWithName(const UWorld* World, const FString& Name, const bool bI
 
 	return nullptr;
 }
+
+UObject* GetAssetByPath(const FString& AssetPath)
+{
+	const FAssetRegistryModule& AssetRegistryModule = FModuleManager::LoadModuleChecked<FAssetRegistryModule>("AssetRegistry");
+	const IAssetRegistry& AssetRegistry = AssetRegistryModule.Get();
+
+	FString NormalizedPath = AssetPath;
+	// Fully-qualified paths should be of the form PackageName.AssetName. If the asset name was not supplied
+	// guess that it is the same as the last element of the package name.
+	if (!NormalizedPath.Contains(TEXT(".")))
+	{
+		const FString AssetName = FPaths::GetBaseFilename(AssetPath);
+		NormalizedPath = FString::Printf(TEXT("%s.%s"), *AssetPath, *AssetName);
+	}
+	
+	const FAssetData AssetData = AssetRegistry.GetAssetByObjectPath(FSoftObjectPath(NormalizedPath));
+	if (AssetData.IsValid())
+	{
+		return AssetData.GetAsset();
+	}
+
+	return nullptr;
+}
