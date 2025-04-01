d64076b431612a9a7518ddf465586581302f3c7e
Peter Melick <165929802+pete-tempo@users.noreply.github.com>
2025-04-01 13:49:12 -0400

    Add Workarounds for Mass Spawn Issues (#230)

---
+++ TempoAgents/Source/TempoAgentsShared/Public/TempoMassSpawner.h
@@ -0,0 +1,54 @@
+// Copyright Tempo Simulation, LLC. All Rights Reserved.
+
+#pragma once
+
+#include "TempoSubsystems.h"
+
+#include "CoreMinimal.h"
+#include "MassSpawner.h"
+#include "TempoMassSpawner.generated.h"
+
+/*
+ * These two classes (UTempoMassSpawnerSubSystem and ATempoMassSpawner) exist to work around two issues in
+ * MassAgentSubsystem and MassSpawner.
+ *
+ * The first issue is that MassAgentSubsystem only handles pending MassAgent component registrations on Tick, meaning
+ * any spawns that happen on BeginPlay will not take into account fragments created by MassAgent components (for
+ * example, obstacle locations).
+ *
+ * The second issue is that MassSpawner checks IsLoaded on all its EntityConfigs and, if they are not loaded, calls
+ * FStreamableManager::RequestAsyncLoad. That will lead to at least a one-frame delay any time EntityConfigs were not
+ * loaded before BeginPlay.
+ *
+ * It just so happens that, in practice, these two issues "cancel out": the agents are not initialized until the first
+ * Tick, but the spawn doesn't actually happen until the first Tick anyhow.
+ *
+ * However, during deferred level loading (see ATempoGameMode::StartPlay), we call
+ * UEngine::BlockTillLevelStreamingCompleted, which causes the EntityConfigs to load and the spawn to happen on
+ * BeginPlay, exposing the consequences of the first issue above.
+ *
+ * We could have intentionally delayed the spawn until the first tick, but we would rather the spawn happen during
+ * BeginPlay, so we added a workaround for each of the above issues here.
+ */
+
+UCLASS()
+class TEMPOAGENTSSHARED_API UTempoMassSpawnerSubSystem : public UTempoGameWorldSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	virtual void OnWorldBeginPlay(UWorld& World) override;
+};
+
+/*
+ * A MassSpawner that is guaranteed to spawn on BeginPlay when bAutoSpawnOnBeginPlay is true,
+ * unlike its parent which may spawn some ticks after.
+ */
+UCLASS()
+class TEMPOAGENTSSHARED_API ATempoMassSpawner : public AMassSpawner
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void BeginPlay() override;
+};
