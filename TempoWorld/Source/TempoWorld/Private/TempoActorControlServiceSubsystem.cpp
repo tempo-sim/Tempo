5942884d8735491da8463d3160ad69bcbe7dc941
Peter Melick <165929802+pete-tempo@users.noreply.github.com>
2025-04-09 21:22:26 -0400

    Add Missing Return When Getting Object Property And ArrayDim != 1 (#237)

--- TempoWorld/Source/TempoWorld/Private/TempoActorControlServiceSubsystem.cpp
+++ TempoWorld/Source/TempoWorld/Private/TempoActorControlServiceSubsystem.cpp
@@ -681,8 +681,10 @@ void GetObjectProperties(const UObject* Object, GetPropertiesResponse& Response)
 			// UProperties can be C-style arrays (who knew?), and this will be indicated by a non-1 ArrayDim member.
 			// For example, FPostProcessSettings has FLinearColor LensFlareTints[8]
 			// We don't support these types
-			Type = TEXT("Unsupported");
+			Type = TEXT("unsupported");
+			return;
 		}
+
 		if (const FStrProperty* StrProperty = CastField<FStrProperty>(Property))
 		{
 			Type = TEXT("string");
