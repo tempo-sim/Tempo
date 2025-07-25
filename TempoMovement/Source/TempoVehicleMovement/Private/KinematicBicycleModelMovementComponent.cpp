7c4ca51e58cdcdefbb3d24e8359adabff2a478f7
Peter Melick <pete@temposimulation.com>
2025-07-25 17:04:21 -0400

    Workingish?

--- TempoMovement/Source/TempoVehicleMovement/Private/KinematicBicycleModelMovementComponent.cpp
+++ TempoMovement/Source/TempoVehicleMovement/Private/KinematicBicycleModelMovementComponent.cpp
@@ -10,7 +10,7 @@ void UKinematicBicycleModelMovementComponent::UpdateState(float DeltaTime, float
 	const float Beta = FMath::Atan2(RearAxleDistance * FMath::Tan(FMath::DegreesToRadians(Steering)),Wheelbase);
 	Velocity = Speed * FVector(FMath::Cos(HeadingAngle + Beta), FMath::Sin(HeadingAngle + Beta), 0.0);
 	AngularVelocity = FMath::RadiansToDegrees(Speed * FMath::Sin(FMath::DegreesToRadians(Steering)) / Wheelbase);
-
+	
 	GetOwner()->AddActorWorldOffset(DeltaTime * Velocity);
 	GetOwner()->AddActorWorldRotation(FRotator(0.0, DeltaTime * AngularVelocity, 0.0));
 }
