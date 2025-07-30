6270bc2c191b793e7ce340bfbd4f4bec08fe707a
Peter Melick <165929802+pete-tempo@users.noreply.github.com>
2025-07-30 00:25:07 -0400

    Add Control Modes to TempoCore (#302)

---
+++ TempoCore/Source/TempoCore/Public/TempoPlayerController.h
@@ -0,0 +1,28 @@
+﻿// Copyright Tempo Simulation, LLC. All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "TempoPlayerController.generated.h"
+
+UCLASS(Blueprintable)
+class TEMPOCORE_API ATempoPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+	ATempoPlayerController();
+
+	virtual void OnPossess(APawn* InPawn) override;
+
+	UFUNCTION(BlueprintCallable, Category = "Tempo")
+	void ToggleMouseCaptured();
+
+	UFUNCTION(BlueprintCallable, Category = "Tempo")
+	void SetMouseCaptured(bool bCaptured);
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Tempo")
+	bool bMouseCaptured = false;
+};
