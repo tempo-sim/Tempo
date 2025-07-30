f0fc32d6a7e559fb207a2868201b307cd42419af
Peter Melick <pete@temposimulation.com>
2025-07-29 21:49:29 -0400

    Add TempoDefaultPawn

---
+++ TempoCore/Source/TempoCore/Private/TempoDefaultPawn.cpp
@@ -0,0 +1,137 @@
+﻿// Copyright Tempo Simulation, LLC. All Rights Reserved
+
+#include "TempoDefaultPawn.h"
+
+#include "GameFramework/Controller.h"
+#include "Components/InputComponent.h"
+#include "Engine/World.h"
+#include "GameFramework/PlayerInput.h"
+
+ATempoDefaultPawn::ATempoDefaultPawn(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	SetCanBeDamaged(true);
+
+	SetRemoteRoleForBackwardsCompat(ROLE_SimulatedProxy);
+	bReplicates = true;
+	NetPriority = 3.0f;
+
+	BaseEyeHeight = 0.0f;
+	bCollideWhenPlacing = false;
+	SpawnCollisionHandlingMethod = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+	// This is the default pawn class, we want to have it be able to move out of the box.
+	bAddDefaultMovementBindings = true;
+
+	BaseTurnRate = 45.f;
+	BaseLookUpRate = 45.f;
+}
+
+void InitializeDefaultPawnInputBindings()
+{
+	static bool bBindingsAdded = false;
+	if (!bBindingsAdded)
+	{
+		bBindingsAdded = true;
+
+		UPlayerInput::AddEngineDefinedAxisMapping(FInputAxisKeyMapping("DefaultPawn_MoveForward", EKeys::W, 1.f));
+		UPlayerInput::AddEngineDefinedAxisMapping(FInputAxisKeyMapping("DefaultPawn_MoveForward", EKeys::S, -1.f));
+		UPlayerInput::AddEngineDefinedAxisMapping(FInputAxisKeyMapping("DefaultPawn_MoveForward", EKeys::Up, 1.f));
+		UPlayerInput::AddEngineDefinedAxisMapping(FInputAxisKeyMapping("DefaultPawn_MoveForward", EKeys::Down, -1.f));
+		UPlayerInput::AddEngineDefinedAxisMapping(FInputAxisKeyMapping("DefaultPawn_MoveForward", EKeys::Gamepad_LeftY, 1.f));
+
+		UPlayerInput::AddEngineDefinedAxisMapping(FInputAxisKeyMapping("DefaultPawn_MoveRight", EKeys::A, -1.f));
+		UPlayerInput::AddEngineDefinedAxisMapping(FInputAxisKeyMapping("DefaultPawn_MoveRight", EKeys::D, 1.f));
+		UPlayerInput::AddEngineDefinedAxisMapping(FInputAxisKeyMapping("DefaultPawn_MoveRight", EKeys::Gamepad_LeftX, 1.f));
+
+		// HACK: Android controller bindings in ini files seem to not work
+		//  Direct overrides here some to work
+#if !PLATFORM_ANDROID
+		UPlayerInput::AddEngineDefinedAxisMapping(FInputAxisKeyMapping("DefaultPawn_MoveUp", EKeys::Gamepad_LeftThumbstick, 1.f));
+		UPlayerInput::AddEngineDefinedAxisMapping(FInputAxisKeyMapping("DefaultPawn_MoveUp", EKeys::Gamepad_RightThumbstick, -1.f));
+		UPlayerInput::AddEngineDefinedAxisMapping(FInputAxisKeyMapping("DefaultPawn_MoveUp", EKeys::Gamepad_FaceButton_Bottom, 1.f));
+		UPlayerInput::AddEngineDefinedAxisMapping(FInputAxisKeyMapping("DefaultPawn_MoveUp", EKeys::LeftControl, -1.f));
+		UPlayerInput::AddEngineDefinedAxisMapping(FInputAxisKeyMapping("DefaultPawn_MoveUp", EKeys::SpaceBar, 1.f));
+		UPlayerInput::AddEngineDefinedAxisMapping(FInputAxisKeyMapping("DefaultPawn_MoveUp", EKeys::C, -1.f));
+		UPlayerInput::AddEngineDefinedAxisMapping(FInputAxisKeyMapping("DefaultPawn_MoveUp", EKeys::E, 1.f));
+		UPlayerInput::AddEngineDefinedAxisMapping(FInputAxisKeyMapping("DefaultPawn_MoveUp", EKeys::Q, -1.f));
+#else
+		UPlayerInput::AddEngineDefinedAxisMapping(FInputAxisKeyMapping("DefaultPawn_MoveUp", EKeys::Gamepad_LeftTriggerAxis, -0.5f));
+		UPlayerInput::AddEngineDefinedAxisMapping(FInputAxisKeyMapping("DefaultPawn_MoveUp", EKeys::Gamepad_RightTriggerAxis, 0.5f));
+#endif
+
+		UPlayerInput::AddEngineDefinedAxisMapping(FInputAxisKeyMapping("DefaultPawn_TurnRate", EKeys::Gamepad_RightX, 1.f));
+		UPlayerInput::AddEngineDefinedAxisMapping(FInputAxisKeyMapping("DefaultPawn_TurnRate", EKeys::Left, -1.f));
+		UPlayerInput::AddEngineDefinedAxisMapping(FInputAxisKeyMapping("DefaultPawn_TurnRate", EKeys::Right, 1.f));
+		UPlayerInput::AddEngineDefinedAxisMapping(FInputAxisKeyMapping("DefaultPawn_Turn", EKeys::MouseX, 1.f));
+
+		UPlayerInput::AddEngineDefinedAxisMapping(FInputAxisKeyMapping("DefaultPawn_LookUpRate", EKeys::Gamepad_RightY, 1.f));
+		UPlayerInput::AddEngineDefinedAxisMapping(FInputAxisKeyMapping("DefaultPawn_LookUp", EKeys::MouseY, -1.f));
+	}
+}
+
+void ATempoDefaultPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	check(PlayerInputComponent);
+
+	if (bAddDefaultMovementBindings)
+	{
+		InitializeDefaultPawnInputBindings();
+
+		PlayerInputComponent->BindAxis("DefaultPawn_MoveForward", this, &ATempoDefaultPawn::MoveForward);
+		PlayerInputComponent->BindAxis("DefaultPawn_MoveRight", this, &ATempoDefaultPawn::MoveRight);
+		PlayerInputComponent->BindAxis("DefaultPawn_MoveUp", this, &ATempoDefaultPawn::MoveUp_World);
+		PlayerInputComponent->BindAxis("DefaultPawn_Turn", this, &ATempoDefaultPawn::AddControllerYawInput);
+		PlayerInputComponent->BindAxis("DefaultPawn_TurnRate", this, &ATempoDefaultPawn::TurnAtRate);
+		PlayerInputComponent->BindAxis("DefaultPawn_LookUp", this, &ATempoDefaultPawn::AddControllerPitchInput);
+		PlayerInputComponent->BindAxis("DefaultPawn_LookUpRate", this, &ATempoDefaultPawn::LookUpAtRate);
+	}
+}
+
+void ATempoDefaultPawn::MoveRight(float Val)
+{
+	if (Val != 0.f)
+	{
+		if (GetController())
+		{
+			FRotator const ControlSpaceRot = GetController()->GetControlRotation();
+
+			// transform to world space and add it
+			AddMovementInput( FRotationMatrix(ControlSpaceRot).GetScaledAxis( EAxis::Y ), Val );
+		}
+	}
+}
+
+void ATempoDefaultPawn::MoveForward(float Val)
+{
+	if (Val != 0.f)
+	{
+		if (GetController())
+		{
+			FRotator const ControlSpaceRot = GetController()->GetControlRotation();
+
+			// transform to world space and add it
+			AddMovementInput( FRotationMatrix(ControlSpaceRot).GetScaledAxis( EAxis::X ), Val );
+		}
+	}
+}
+
+void ATempoDefaultPawn::MoveUp_World(float Val)
+{
+	if (Val != 0.f)
+	{
+		AddMovementInput(FVector::UpVector, Val);
+	}
+}
+
+void ATempoDefaultPawn::TurnAtRate(float Rate)
+{
+	// calculate delta for this frame from the rate information
+	AddControllerYawInput(Rate * BaseTurnRate * GetWorld()->GetDeltaSeconds() * CustomTimeDilation);
+}
+
+void ATempoDefaultPawn::LookUpAtRate(float Rate)
+{
+	// calculate delta for this frame from the rate information
+	AddControllerPitchInput(Rate * BaseLookUpRate * GetWorld()->GetDeltaSeconds() * CustomTimeDilation);
+}
