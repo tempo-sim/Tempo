8dd325a39a7599cd695e9d15d83b0c069ba353a7
jason-tempo <jason@temposimulation.com>
2025-08-18 14:01:11 -0400

    Scrollable List Feature Added

---
+++ TempoCore/Source/TempoCore/Private/TempoPawnEntryWidget.cpp
@@ -0,0 +1,40 @@
+﻿// TempoPawnEntryWidget.cpp
+#include "TempoPawnEntryWidget.h"
+#include "Components/TextBlock.h"
+#include "Components/Button.h"
+#include "GameFramework/Pawn.h"
+#include "Kismet/GameplayStatics.h"
+#include "TempoPlayerController.h" // Include your player controller header
+
+void UTempoPawnEntryWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	// Cache the player controller for later use.
+	TempoPlayerController = Cast<ATempoPlayerController>(UGameplayStatics::GetPlayerController(GetWorld(), 0));
+
+	// Bind our C++ function to the button's OnClicked event.
+	if (PossessButton)
+	{
+		PossessButton->OnClicked.AddDynamic(this, &UTempoPawnEntryWidget::OnPossessButtonClicked);
+	}
+}
+
+void UTempoPawnEntryWidget::SetPawn(APawn* InPawn)
+{
+	RepresentedPawn = InPawn;
+
+	if (PawnNameText && RepresentedPawn)
+	{
+		PawnNameText->SetText(FText::FromString(RepresentedPawn->GetActorLabel()));
+	}
+}
+
+void UTempoPawnEntryWidget::OnPossessButtonClicked()
+{
+	if (RepresentedPawn && TempoPlayerController)
+	{
+		TempoPlayerController->CacheAIController(RepresentedPawn);	
+		TempoPlayerController->Possess(RepresentedPawn);
+	}
+}
---
+++ TempoCore/Source/TempoCore/Private/TempoPawnListWidget.cpp
@@ -0,0 +1,43 @@
+﻿#include "TempoPawnListWidget.h"
+#include "TempoPlayerController.h"
+#include "Components/ScrollBox.h"
+#include "GameFramework/Pawn.h"
+#include "Kismet/GameplayStatics.h"
+#include "TempoPawnEntryWidget.h"
+#include "Logging/LogMacros.h" // --- MODIFICATION: Include for logging ---
+
+void UTempoPawnListWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+	TempoPlayerController = Cast<ATempoPlayerController>(UGameplayStatics::GetPlayerController(GetWorld(), 0));
+
+	if (TempoPlayerController)
+	{
+		TempoPlayerController->OnPawnListUpdated.AddDynamic(this, &UTempoPawnListWidget::RefreshPawnList);
+	}
+	RefreshPawnList();
+}
+
+void UTempoPawnListWidget::RefreshPawnList()
+{
+	if (!TempoPlayerController || !PawnEntryScrollBox || !ListItemWidgetClass)
+	{
+		return;
+	}
+	AllPossessablePawns = TempoPlayerController->GetAllPossessablePawns();
+	PawnEntryScrollBox->ClearChildren();
+
+	for (APawn* Pawn : AllPossessablePawns)
+	{
+		UTempoPawnEntryWidget* ListItem = CreateWidget<UTempoPawnEntryWidget>(this, ListItemWidgetClass);
+		if (ListItem)
+		{
+			// --- MODIFICATION START ---
+			// Log the name of the pawn being added to the list for debugging.
+			UE_LOG(LogTemp, Warning, TEXT("UTempoPawnListWidget: Adding pawn '%s' to list."), *Pawn->GetActorNameOrLabel());
+			// --- MODIFICATION END ---
+			ListItem->SetPawn(Pawn);
+			PawnEntryScrollBox->AddChild(ListItem);
+		}
+	}
+}
