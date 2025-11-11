6de0d703b80189f3844c351035e6eb8c70d258db
Peter Melick <pete@temposimulation.com>
2025-11-11 09:51:09 -0500

    Rewrite GenProtos Prebuild Step as Python

--- TempoCore/Scripts/PreBuild.bat
+++ TempoCore/Scripts/PreBuild.bat
@@ -11,8 +11,6 @@ if defined TEMPO_SKIP_PREBUILD (
 )
 
 REM Simply call the individual scripts from the same directory
-bash %~dp0GenProtos.sh %*
-if %errorlevel% neq 0 exit /b %errorlevel%
 bash %~dp0GenAPI.sh %1 %3 %4
 if %errorlevel% neq 0 exit /b %errorlevel%
 
