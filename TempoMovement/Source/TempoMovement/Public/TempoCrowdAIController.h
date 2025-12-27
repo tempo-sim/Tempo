add39399d725faee1d73cd90e567b256c1c7ae85
jason-tempo <jason@temposimulation.com>
2025-12-26 23:39:37 -0500

    Tempo Crowd AI Controller (#359)

---
+++ TempoMovement/Source/TempoMovement/Public/TempoCrowdAIController.h
@@ -0,0 +1,51 @@
+// Copyright Tempo Simulation, LLC. All Rights Reserved
+
+#pragma once
+
+#include "AIController.h"
+#include "CoreMinimal.h"
+#include "DetourCrowdAIController.h"
+
+#include "TempoCrowdAIController.generated.h"
+
+UENUM(BlueprintType)
+enum class ETempoAvoidanceQuality : uint8
+{
+	Low    UMETA(DisplayName = "Low"),
+	Medium UMETA(DisplayName = "Medium"),
+	Good   UMETA(DisplayName = "Good"),
+	High   UMETA(DisplayName = "High")
+};
+
+UCLASS()
+class TEMPOMOVEMENT_API ATempoCrowdAIController : public AAIController
+{
+	GENERATED_BODY()
+
+public:
+	ATempoCrowdAIController(const FObjectInitializer& ObjectInitializer);
+
+protected:
+	virtual void OnPossess(APawn* InPawn) override;
+
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(EditDefaultsOnly, Category = "Tempo")
+	TObjectPtr<UBehaviorTree> BehaviorTreeAsset;
+    
+	UPROPERTY(EditDefaultsOnly, Category = "Tempo")
+	TObjectPtr<UBlackboardData> BlackboardAsset;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Tempo")
+	bool bEnableCrowdSeparation = true;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Tempo")
+	float CrowdSeparationWeight = 50.0f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Tempo")
+	float AvoidanceRangeMultiplier = 1.1f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Tempo")
+	ETempoAvoidanceQuality AvoidanceQuality = ETempoAvoidanceQuality::Medium;
+};
