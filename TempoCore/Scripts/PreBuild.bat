11f724b03c9fbe8077ff20fb58d2cf53754b2a80
craigdelancy-ServeRobotics <craig.delancy@serverobotics.com>
2026-02-02 09:28:00 -0500

    Sync Tempo prebuild pipeline with upstream (PR #380)

--- TempoCore/Scripts/PreBuild.bat
+++ TempoCore/Scripts/PreBuild.bat
@@ -2,8 +2,6 @@
 REM Copyright Tempo Simulation, LLC. All Rights Reserved
 
 REM Simply call the individual scripts from the same directory
-bash %~dp0GenProtos.sh %*
-if %errorlevel% neq 0 exit /b %errorlevel%
 bash %~dp0GenAPI.sh %1 %3 %4
 if %errorlevel% neq 0 exit /b %errorlevel%
 
