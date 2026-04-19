9aea01d67a92402889728784dafa6ecd37b69cd5
Peter Melick <pete@temposimulation.com>
2026-04-19 17:31:25 -0400

    Fix Default Settings

--- TempoSensors/Source/TempoSensorsShared/Private/TempoSensorsSettings.cpp
+++ TempoSensors/Source/TempoSensorsShared/Private/TempoSensorsSettings.cpp
@@ -3,12 +3,30 @@
 #include "TempoSensorsSettings.h"
 
 UTempoSensorsSettings::UTempoSensorsSettings()
-	:
-CameraPostProcessMaterialNoDepth(FSoftObjectPath(TEXT("/TempoSensors/Materials/M_CameraPostProcess_NoDepth.M_CameraPostProcess_NoDepth"))),
-CameraPostProcessMaterialWithDepth(FSoftObjectPath(TEXT("/TempoSensors/Materials/M_CameraPostProcess_WithDepth.M_CameraPostProcess_WithDepth"))),
-LidarPostProcessMaterial(FSoftObjectPath(TEXT("/TempoSensors/Materials/M_LidarPostProcess.M_LidarPostProcess")))
 {
 	CategoryName = TEXT("Tempo");
+}
+
+void UTempoSensorsSettings::PostInitProperties()
+{
+	Super::PostInitProperties();
+
+	auto SetIfNull = [](TSoftObjectPtr<UMaterialInterface>& Ptr, const TCHAR* Path)
+	{
+		if (Ptr.IsNull())
+		{
+			Ptr = FSoftObjectPath(Path);
+		}
+	};
+
+	SetIfNull(CameraPostProcessMaterialNoDepth, TEXT("/TempoSensors/Materials/M_TempoCamera_Distort_NoDepth.M_TempoCamera_Distort_NoDepth"));
+	SetIfNull(CameraPostProcessMaterialWithDepth, TEXT("/TempoSensors/Materials/M_TempoCamera_Distort_WithDepth.M_TempoCamera_Distort_WithDepth"));
+	SetIfNull(CameraStitchPassthroughMaterial, TEXT("/TempoSensors/Materials/M_TempoStitch_Passthrough.M_TempoStitch_Passthrough"));
+	SetIfNull(CameraStitchAuxMaterial, TEXT("/TempoSensors/Materials/M_TempoStitch_Aux.M_TempoStitch_Aux"));
+	SetIfNull(CameraStitchMergeMaterialWithDepth, TEXT("/TempoSensors/Materials/M_TempoStitch_Merge_WithDepth.M_TempoStitch_Merge_WithDepth"));
+	SetIfNull(CameraStitchMergeMaterialNoDepth, TEXT("/TempoSensors/Materials/M_TempoStitch_Merge_NoDepth.M_TempoStitch_Merge_NoDepth"));
+	SetIfNull(CameraProxyTonemapMaterial, TEXT("/TempoSensors/Materials/M_TempoStitch_Proxy.M_TempoStitch_Proxy"));
+	SetIfNull(LidarPostProcessMaterial, TEXT("/TempoSensors/Materials/M_LidarPostProcess.M_LidarPostProcess"));
 }
 
 #if WITH_EDITOR
